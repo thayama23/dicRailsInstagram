f5f9d2b89856f1d2e90230dc314d6277a6f18f95
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-02-13 15:27:16 +0900

    username is added to blogs/index

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,11 +2,15 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   def create
-    @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blogs_path, notice: "ブログを作成しました！"
-    else
+    @blog = current_user.blogs.build(blog_params)
+    if params[:back]
       render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
     end
   end
 
@@ -32,13 +36,18 @@ class BlogsController < ApplicationController
 
   def index
     @blogs = Blog.all
+
+  
   end
 
   def show
   end
 
   def confirm
-    @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
+    render :new if @blog.invalid?
+    # @blog = Blog.new(blog_params)
+    # @blog.user_id = current_user.id #現在ログインしているuserのidを、blogのuser_idカラムに挿入する
   end
 
   private
