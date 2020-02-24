601fdcd66f3197761678b8584177807626488dc4
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-02-24 11:58:43 +0900

    config/app.rbへ追記、config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module SessionLogin
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
